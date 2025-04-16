88abd7c87dc19dafef331261d87367127329c7aa
kushwahawakeel12 <wakeelkushwaha1212@gmail.com>
2025-04-16 09:42:31 +0530

    first commit

---
+++ src/main/java/com/wakeel/myfirstA/MyfirstAApplication.java
@@ -0,0 +1,16 @@
+package com.wakeel.myfirstA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstAApplication.class, args);
+		System.out.println("Hello world how are you ");
+		System.out.println("radhe radhe");
+		
+	}
+
+}
